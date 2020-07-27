56258e222774411f90e196556c854713306a5fcb
Vitaly <dubovetsvitaliy@gmail.com>
2020-07-27 11:58:18 +0500

    Added new models for business logic of getting orders

--- pizza_boom/pizza_orders/api_resources/api_pizza_orders.py
+++ pizza_boom/pizza_orders/api_resources/api_pizza_orders.py
@@ -1,11 +1,10 @@
-from typing import List, OrderedDict
-
 import flask
 import structlog
 from flask_restful import Resource
 
-from pizza_boom.pizza_orders.db_models.pizza_order_models import PizzaOrder, PizzaStatus
-from pizza_boom.pizza_orders.schemas.pizza_order import PizzaOrderSchema
+from pizza_boom.pizza_orders.business_logic.get_pizza_order import (
+    get_pizza_orders_with_query_params
+)
 
 
 logger = structlog.get_logger()
@@ -27,17 +26,7 @@ class PizzaOrdersAPI(Resource):
         )
 
         query_params: dict = flask.request.args
-        limit: str = query_params.get('limit') if query_params.get('limit') else None
-        status: str = (
-            query_params.get('status') if query_params.get('status') else PizzaStatus.RECEIVED
+        response, status_code = get_pizza_orders_with_query_params(
+            query_params=query_params
         )
-
-        if limit and isinstance(limit, str):
-            limit: int = int(limit)
-
-        pizza_orders: List[PizzaOrder] = [
-            item for item in PizzaOrder.scan(PizzaOrder.status == status, limit=limit)
-        ]
-        pizza_orders_data: dict = PizzaOrderSchema().dump(pizza_orders, many=True)
-
-        return pizza_orders_data, 200
+        return response, status_code
---
+++ pizza_boom/pizza_orders/business_logic/get_pizza_order.py
@@ -0,0 +1,27 @@
+from typing import List
+
+import flask
+import structlog
+
+from pizza_boom.pizza_orders.db_models.pizza_order_models import PizzaOrder, PizzaStatus
+from pizza_boom.pizza_orders.schemas.pizza_order import PizzaOrderSchema
+
+
+logger = structlog.get_logger()
+
+
+def get_pizza_orders_with_query_params(query_params: dict):
+    limit: str = query_params.get('limit') if query_params.get('limit') else None
+    status: str = (
+        query_params.get('status') if query_params.get('status') else PizzaStatus.RECEIVED
+    )
+
+    if limit and isinstance(limit, str):
+        limit: int = int(limit)
+
+    pizza_orders: List[PizzaOrder] = [
+        item for item in PizzaOrder.scan(PizzaOrder.status == status, limit=limit)
+    ]
+    pizza_orders_data: dict = PizzaOrderSchema().dump(pizza_orders, many=True)
+
+    return pizza_orders_data, 200
